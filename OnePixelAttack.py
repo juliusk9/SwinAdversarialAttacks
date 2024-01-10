f9babd1bfbffe724428c2b91dc631abcf5a2fbbc
Julius <j.s.kooistra@student.utwente.nl>
2024-01-10 17:35:49 +0100

    Initial commit

---
+++ OnePixelAttack.py
@@ -0,0 +1,169 @@
+import os
+import torch
+from utils import save_image, make_dirs
+
+from scipy.optimize import differential_evolution
+
+
+# Maybe function not needed because we have our own perturb image function.
+def perturb_image_one_pixel(x, img):
+    img = torch.clone(img)
+
+    y_pos, x_pos, *rgb = x
+
+    rgb_tensor = torch.tensor(rgb, dtype=img.dtype)
+
+    img[0, :, int(y_pos), int(x_pos)] = rgb_tensor
+
+    return img
+ 
+
+
+def probability_classes(perturbation, image, org_class, model, device):
+    # For debugging purposes
+    #print("Perturbation in prob_clas", perturbation)
+    #print("Image at pert pixels", image[0, :, int(perturbation[0]), int(perturbation[1])])
+
+    # Perturb image and store in pert_image
+    pert_image = perturb_image_one_pixel(perturbation, torch.clone(image))
+
+    #print("Org Image after perturbation", image[0, :, int(perturbation[0]), int(perturbation[1])])
+    #print("Perturbed image", pert_image[0, :, int(perturbation[0]), int(perturbation[1])])
+
+    #print("Dimensions of ")
+    #print(np.shape(pert_image))
+    #print(np.shape(image))
+
+    comparison = torch.eq(image[0], pert_image[0])
+
+    if len(torch.nonzero(~comparison)) == 0:
+        print("No difference between image and perturbed image")
+
+
+    #print("number of pixels not equal:", [x for x in torch.eq(image, pert_image) if not x])
+    
+    output = model(torch.Tensor(pert_image).to(device))
+
+    probabilities = torch.nn.functional.softmax(output, dim=1)
+
+    adjusted_probability = probabilities[0][org_class]
+
+    #print("Adj prob:", adjusted_probability)
+
+    return adjusted_probability.to('cpu').detach()
+
+
+def predict(perturbation, image, org_class, model, device, image_list, minimize=True):
+
+    print(len(perturbation))
+    print(perturbation)
+
+    image_list.append(image)
+
+
+    if len(image_list) > 1:
+        print("Checking original images")
+        for i in range(1, len(image_list)):
+            if len(torch.nonzero(~torch.eq(image_list[i][0], image_list[i-1][0]))) > 0:
+                print("Source images are not the same")
+                print(torch.nonzero(~torch.eq(image_list[i][0], image_list[i-1][0])))
+        image_list.pop(0)
+
+    #print("OriginalImage", image)
+
+    attacked_image = perturb_image_one_pixel(perturbation, image)
+
+    comparison = torch.eq(image[0], attacked_image[0])
+
+    if len(torch.nonzero(~comparison)) == 0:
+        print("No difference between image and perturbed image")
+
+
+    #print("Attacked_image", attacked_image)
+    #print("Tensored Attackedimage", torch.Tensor(attacked_image))
+
+    original_output = model(image.to(device))
+    perturbed_output = model(attacked_image.to(device))
+
+    print("Original", original_output[0][0])
+    print("Perturbed", perturbed_output[0][0])
+
+    return perturbed_output[0][org_class].item()
+
+
+def attack_success(perturbation, image, org_class, model, device):
+
+    attacked_image = perturb_image_one_pixel(perturbation, torch.clone(image))
+
+    print("Checking if succesful")
+    confidences = model(attacked_image.to(device))[0]
+    predicted_class = torch.argmax(confidences)
+
+    print(confidences)
+    print(predicted_class, org_class)
+
+    if predicted_class != org_class:
+        return True
+
+
+def attack(index, model, device, image, label, pixel_count=1, maxiter=50, popsize=10):
+    make_dirs("advanced_one_pixel")
+    # # Returns image from the test dataset
+    # sample = dataset[index]
+
+    # # Image information
+    # org_img = sample[0].unsqueeze(0)
+    org_class = torch.argmax(label).item()
+
+    #print(torch.eq(image, image))
+
+    # Clean up memory
+    torch.cuda.empty_cache()
+ 
+    model.to(device)
+
+    max_pixel_val = 10
+    min_pixel_val = -10
+
+    bounds = [[(0, 256), (0,256), (min_pixel_val ,max_pixel_val), (min_pixel_val ,max_pixel_val), (min_pixel_val ,max_pixel_val)] * pixel_count]
+    # The population has size popsize * (N - N_equal)
+    popsize = popsize // len(bounds[0])
+
+    # This is the function taken by the differential evolution of SciPy to find the minimum of.
+    image_list = []
+    def func(perturb):
+        #print("\n Called as func")
+        return predict(perturb, torch.clone(image), org_class, model, device, image_list)
+    
+    # Function that keeps track of the best solution found so far.
+    def callback(x, convergence=None):
+        print("\n Called as callback")
+        return attack_success(x, torch.clone(image), org_class, model, device)
+
+    result = differential_evolution(
+        # removed from below: recombination=1, atol=-1, 
+        func, bounds[0], maxiter=maxiter, popsize=popsize, recombination=1, atol=-1, 
+        callback=callback, polish=False
+    )
+
+    print("Result", result.message)
+    print("Success", result.success)
+
+    print("\n DE Finished")
+
+    model.to('cpu')
+
+    pert_image = perturb_image_one_pixel(result.x, torch.clone(image))
+
+    pert_pred = torch.nn.functional.softmax(model(torch.Tensor(pert_image)), dim=1)
+    pert_class = torch.argmax(pert_pred).detach().numpy()
+
+    print(org_class, pert_class)
+    
+    successful = pert_class != org_class
+
+    if successful:
+        save_image(pert_image.squeeze(), os.path.join(os.getcwd(), "dataset", "advanced_one_pixel", str(index) + "-attacked.png"))
+        save_image(image.squeeze(), os.path.join(os.getcwd(), "dataset", "advanced_one_pixel", str(index) + "-original.png"))
+
+    return successful
---
+++ utils.py
@@ -0,0 +1,278 @@
+# utils
+import os
+import shutil
+import json
+import glob
+
+# images
+import cv2
+import albumentations
+import albumentations.pytorch
+
+# Torch
+import torch
+import timm
+from torch.utils.data import Dataset
+import torch.nn as nn
+from torchvision import transforms
+import torch.optim as optim
+
+
+################################ Variables ################################
+classes = ["A", "F", "TA", "PT", "DC", "LC", "MC", "PC"]
+zooms = ["40", "100", "200", "400"]
+
+
+################################ Classes ################################
+class My_data(Dataset):
+    def __init__(self, data, transforms=None, setnp=False):
+        self.image_list = data
+        self.data_len = len(self.image_list)
+        self.transforms = transforms
+        self.eicls = ["A", "F", "TA", "PT", "DC", "LC", "MC", "PC"]
+
+    def __getitem__(self, index):       
+        current_image_path = self.image_list[index]
+        im_as_im = cv2.imread(current_image_path)
+        if im_as_im is None:
+            raise ValueError(f"Image not found or is corrupted: {current_image_path}")
+        im_as_im = cv2.cvtColor(im_as_im, cv2.COLOR_BGR2RGB)
+
+        # Perform label encoding for multi-label classification
+        parts = current_image_path.split('_')[-1].split('-')
+        if parts[2]=="13412":
+            labels =[0,0,0,0,1,1,0,0]
+        else:
+            labels = [int(label == parts[0]) for label in self.eicls]
+        labels = torch.tensor(labels)
+
+        if self.transforms is not None:
+            augmented = self.transforms(image=im_as_im)
+            im_as_im = augmented['image']
+            #print(type(im_as_im))
+
+        return (im_as_im, labels)
+    
+    def __getname__(self, index):
+        return self.image_list[index].split(os.sep)[-1]
+    
+    def __getzoom__(self, index):
+        return self.image_list[index].split("_")[-1].split("-")[3]
+    
+    def __getclass__(self, index):
+        return self.image_list[index].split("_")[-1].split("-")[0]
+
+    def __len__(self):
+        return self.data_len
+    
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2.0, class_weights=None):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.class_weights = class_weights
+
+    def forward(self, logits, labels):
+        probs = torch.sigmoid(logits)
+        ce_loss = nn.BCELoss()(probs, labels)
+        weight = (1 - probs).pow(self.gamma)
+        loss = ce_loss  # Initialize loss with cross-entropy loss
+        if self.class_weights is not None:
+            weight = weight * self.class_weights
+            loss = loss * weight
+        return loss
+    
+class CustomTransforms():
+    def __init__(self, files):
+        self.transform = {
+            'train': albumentations.Compose([
+                albumentations.Resize(256, 256),
+                albumentations.OneOf([
+                                    albumentations.HorizontalFlip(),
+                                    albumentations.Rotate(limit=45),
+                                    albumentations.VerticalFlip(),
+                                    albumentations.GaussianBlur(),
+                                    albumentations.NoOp()
+                ], p=1),
+                albumentations.Normalize(mean=(0.787, 0.625, 0.765),
+                                std=(0.105, 0.138, 0.089), p=1),
+                albumentations.pytorch.transforms.ToTensorV2()
+            ]),
+
+            'valid': albumentations.Compose([
+                albumentations.Resize(256, 256),
+                albumentations.Normalize(mean=(0.786, 0.623, 0.766),
+                                std=(0.105, 0.138, 0.089), p=1),
+                albumentations.pytorch.transforms.ToTensorV2()
+            ]),
+
+            'test': None,
+
+            'resize': albumentations.Compose([
+                albumentations.Resize(256, 256),
+            ]),
+
+            "resize_tensor": albumentations.Compose([
+                albumentations.Resize(256, 256),
+                albumentations.pytorch.transforms.ToTensorV2()
+            ])
+        }
+        self.img_set = My_data(files, transforms=self.transform["resize_tensor"])
+
+        ## TODO: revisit this, the means are not necessarily the means of the pixel values per chanel
+        mean_list = [0, 0, 0]
+        std_list = [0, 0, 0]
+        for img in self.img_set:
+            for i, rgb in enumerate(img[0]):
+                rgb = rgb.float()
+                mean_list[i] += torch.mean(rgb)
+                std_list[i] += torch.std(rgb)
+
+        n = len(self.img_set) * 255
+        means = [x/n for x in mean_list]
+        stds = [x/n for x in std_list]
+
+        self.transform["test"] = albumentations.Compose([
+            albumentations.Resize(256, 256),
+            albumentations.Normalize(mean=tuple([mean.item() for mean in means]), std=tuple([std.item() for std in stds])),
+            albumentations.pytorch.transforms.ToTensorV2(),
+        ])
+        del self.img_set
+
+    def get_transform(self, transformer):
+        try:
+            return self.transform[transformer]
+        except KeyError:
+            print("Transformer does not exist")      
+    
+
+################################ Functions ################################
+# Takes all images from a train/test dict and copies them to new destination for easy access
+def copy_images(image_dict, type):
+
+    # Get current working directory
+    current_dir = os.getcwd()
+
+    # Check whether the new file structure is already initialised
+    if not os.path.isdir(os.path.join(current_dir, "dataset", "train", "TA", "400")) or not os.path.isdir(os.path.join(current_dir, "dataset", "test", "TA", "400")):
+        for c in classes:
+            for z in zooms:
+                os.makedirs(os.path.join(os.getcwd(), "dataset", "test", c, z))
+                os.makedirs(os.path.join(os.getcwd(), "dataset", "train", c, z))
+
+    for c in classes:
+        for z in zooms:
+
+            # Destination path
+            dst = os.path.join(current_dir, "dataset", type, c, z)
+
+            # Remove all images currently in destination path
+            for file in os.listdir(dst):
+                os.remove(os.path.join(dst, file))
+
+            # Copy all images from BreaKHis to dataset directory
+            for image in image_dict[c][z]:
+                src = os.path.join(image)
+                dst = os.path.join(current_dir, "dataset", type, c, z)
+
+                shutil.copy2(src, dst)
+
+def make_dirs(name):
+    if not os.path.isdir(os.path.join(os.getcwd(), "dataset", name, "TA", "400")):
+        for c in classes:
+            for z in zooms:
+                os.makedirs(os.path.join(os.getcwd(), "dataset", name, c, z))
+
+
+def get_dict(filepath):
+    with open(os.path.join(os.getcwd(), "txt", filepath)) as f:
+        print("Opening Train.txt")
+        return json.load(f)
+    
+
+def get_files(filepath):
+    return glob.glob(filepath)
+    
+
+def check_corrupted_imgs(train_dict, test_dict):
+    """This code checks the image paths for a correct split. If the split seems corrupted, the function calls copy_images which ensures a proper split.
+    Input: the train dict retreived from the text file."""
+    correct = True
+    copied_files = glob.glob("./dataset/train/A/40/*.png")
+    copied_files = [path.split("_")[-1] for path in copied_files]
+
+    if len(train_dict["A"]["40"]) != len(copied_files):
+        print("Not as many A-40 train files in dict as in dataset directory")
+        print("Therefore current copy not correct")
+        correct = False
+    else:
+        for path in train_dict["A"]["40"]:
+            if path.split("_")[-1] not in copied_files:
+                print("File {} found in train dict, but not in copied files.".format(path.split("_")[-1]))
+                print("Therefore current copy not correct")
+                correct = False
+
+    if not correct:
+        print("Current copy does not satisfy.")
+        print("Copying images from BreaKHis to dataset/** directory according to train_dict and test_dict.")
+
+        # Copy images to correct directory for easy access
+        copy_images(train_dict, "train")
+        copy_images(test_dict, "test")
+
+    return correct
+    
+
+def save_image(tensor, path):
+    # Transform tensor to PIL Image
+    image = transforms.ToPILImage()(tensor)
+
+    # Save image
+    image.save(path)
+
+
+def perturb_image(perturbation, img):
+    y_pos, x_pos, *rgb = perturbation
+    
+    img[y_pos, x_pos] = rgb
+
+    return img
+
+
+def get_class_weigths(image_dict: dict):
+    # Number of samples in each class
+    # Assumption is made that in each fold, these are the same (since we do stratified split)
+    class_samples = [sum([len(z) for z in c.values()]) for c in image_dict.values()]
+
+    # Hardcoded from paper
+    # class_samples = [367, 803, 456, 370, 2763, 492, 629, 449]  # Number of samples in each class for training
+
+    total_samples = sum(class_samples)
+    samples = total_samples/len(class_samples)
+    class_weights = [samples / (s + 1e-8) for s in class_samples]
+    class_weights = torch.tensor(class_weights)
+    return class_weights
+
+
+def get_model(device, image_dict: dict):
+    torch.cuda.empty_cache()
+
+    model = timm.create_model(
+        'swinv2_tiny_window8_256.ms_in1k',
+        pretrained=False,
+        features_only=False,
+        num_classes = 8,
+        drop_path_rate=0.2
+    )
+
+    class_weights = get_class_weigths(image_dict).to(device)
+    criterion = FocalLoss(class_weights)
+    optimizer = optim.AdamW(
+        filter(lambda p: p.requires_grad, model.parameters()), 
+        lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.5)
+    checkpoint1 = torch.load('Master.pth', map_location=torch.device(device))
+    model.load_state_dict(checkpoint1['model_state_dict'])
+    model = model.to(device)
+    return model
+
